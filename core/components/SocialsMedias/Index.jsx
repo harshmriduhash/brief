3e30678a4776010ce2929581e22335c784c76141
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-02 12:35:39 +0530

    updating

--- core/components/SocialsMedias/Index.jsx
+++ core/components/SocialsMedias/Index.jsx
@@ -1,45 +1,30 @@
-
 //components
-import SocialMediaItem from "./SocialMediaItem"
-import Icons from "../Icons"
+import SocialMediaItem from "./SocialMediaItem";
+import Icons from "../Icons";
 
 const SocialMedias = ({ spacing, className }) => {
+  return (
+    <>
+      <ul className={`d-flex socials-medias ${className}`}>
+        <li className="text-primary socials-medias-item">
+          <a href="https://github.com/harshmriduhash">
+            <SocialMediaItem icon={<Icons.GitHub size="3rem" />} />
+          </a>
+        </li>
+      </ul>
+      <style jsx>{`
+        .socials-medias {
+          position: relative;
+          margin-top: ${spacing ? "16rem" : "0"};
 
-    return (
-        <>
-            <ul className={`d-flex socials-medias ${className}`}>
-                <li className="text-primary socials-medias-item">
-                    <a href="/">
-                        <SocialMediaItem icon={<Icons.GitHub size="3rem" />} />
-                    </a>
-                </li>
-                <li className="text-primary socials-medias-item">
-                    <a href="/">
-                        <SocialMediaItem icon={<Icons.Twitter size="3rem" />} />
-                    </a>
-                </li>
-                <li className="text-primary socials-medias-item">
-                    <a href="/">
-                        <SocialMediaItem icon={<Icons.Word size="3rem" />} />
-                    </a>
-                </li>
-            </ul>
-            <style jsx>{`
-                
-                .socials-medias{
-                    position: relative;
-                    margin-top: ${spacing ? '16rem' : '0'};
-                
-                    z-index: 100;
-                }
-                .socials-medias-item{
-                    margin-right: 3rem;
-                }
-            
-            `}</style>
-        </>
-    )
-}
+          z-index: 100;
+        }
+        .socials-medias-item {
+          margin-right: 3rem;
+        }
+      `}</style>
+    </>
+  );
+};
 
-
-export default SocialMedias
+export default SocialMedias;
